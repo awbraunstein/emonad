eac37a9e8ab403b4c9f442ad0229ac43e6ac1f64
Rafe Kettler <rafe.kettler@gmail.com>
2012-12-10 23:52:40 -0500

    Remove dead code: using vty builtin layout, we don't need to roll our own system

--- src/UI.hs
+++ src/UI.hs
@@ -5,25 +5,6 @@ module UI where
 import Graphics.Vty
 import BufferList
 
-type Location = (Int, Int)
-
-class Drawable d where
-  draw :: Location -> d -> IO ()
-
-data UI where
-  Widget :: (Drawable d) => Location -> d -> UI
-  UIList :: [UI] -> UI
-
-instance Drawable String where
-  draw (x,y) s = undefined
-
-instance Drawable UI where
-  draw _ (Widget l w) = draw l w
-  draw l (UIList uis) = mapM_ (draw l) uis
-
-drawUI :: UI -> IO ()
-drawUI = draw (0,0)
-
 drawEditor :: BufferList -> Vty -> IO ()
 drawEditor bs v = update v $ pic_for_image screen where
   reverseAttr = with_style current_attr reverse_video
