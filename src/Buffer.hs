964bb64d9f20e971136d8e8d3c1ef498c747b8a2
Andrew Braunstein <awbraunstein@gmail.com>
2012-12-13 22:58:06 -0500

    previous and next actually work correctly now

--- src/Buffer.hs
+++ src/Buffer.hs
@@ -46,16 +46,21 @@ moveBackward = move (-1)
 
 -- | Move to the same position in the previous line.
 movePrevious :: Buffer -> Buffer
-movePrevious = moveBackwardToDelimiter '\n'
+movePrevious b@(B _ t p _ _) =
+  let start = case R.elemIndexBackward t p '\n' of
+        Nothing -> 0
+        Just x -> (x + 1) in
+  let b' = (moveToBeginningOfLine . moveBackward . moveToBeginningOfLine) b in
+  moveForwardUntilDelimiter ((point b) - start ) '\n' b'
 
 -- | Move to the same position in the next line.
 moveNext :: Buffer -> Buffer
 moveNext b@(B n t p m _) =
   let start = case R.elemIndexBackward t p '\n' of
                 Nothing -> 0
-                Just x -> x in
-  let b'@(B n t' p' m' pg') = moveToEndOfLine b in
-  moveForwardUntilDelimiter (p - start) '\n' b'
+                Just x -> (x + 1) in
+  let b' = (moveForward . moveToEndOfLine) b in
+  moveForwardUntilDelimiter ((point b) - start) '\n' b'
 
 -- | Delete a character at an index.
 deleteChar :: Int -> Buffer -> Buffer
@@ -100,23 +105,38 @@ moveForwardUntilDelimiter n c b@(B _ t p _ _) =
                else
                  moveForwardUntilDelimiter (n-1) c (moveForward b)
 
+-- | Move backward n times or until you see the specified character
+moveBackwardUntilDelimiter :: Int -> Char -> Buffer -> Buffer
+moveBackwardUntilDelimiter 0 _ b = b
+moveBackwardUntilDelimiter n c b@(B _ t p _ _) =
+  case R.index p t of
+    Nothing -> b
+    Just c' -> if c' == c then
+                 b
+               else
+                 moveBackwardUntilDelimiter (n-1) c (moveBackward b)
+
 -- | Move the point forward to the next instance of a character, the delimiter.
---   If the character doesn't exist after the point, don't move the point.
+--   If the character doesn't exist after the point, move to the end of the file.
 moveForwardToDelimiter :: Char -> Buffer -> Buffer
 moveForwardToDelimiter c b@(B n t p m pg) = case R.elemIndexForward t p c of
-  Nothing -> b
+  Nothing -> B n t (R.length t) m pg
   Just x -> B n t x m pg
 
 -- | Move the point backward to the last instance of a character, the delimiter.
---   If the character doesn't exist before the point, don't move the point.
+--   If the character doesn't exist before the point, move to the start of the file.
 moveBackwardToDelimiter :: Char -> Buffer -> Buffer
 moveBackwardToDelimiter c b@(B n t p m pg) = case R.elemIndexBackward t p c of
-  Nothing -> b
+  Nothing -> B n t 0 m pg
   Just x -> B n t x m pg
 
 -- | Move to the beginning of the current line.
 moveToBeginningOfLine :: Buffer -> Buffer
-moveToBeginningOfLine = moveForward . moveBackwardToDelimiter '\n'
+moveToBeginningOfLine b = let b' = moveBackwardToDelimiter '\n' b in
+                          if point b' == 0 then
+                            b'
+                          else
+                            moveForward b'
 
 -- | Move to the end of the current line.
 moveToEndOfLine :: Buffer -> Buffer
